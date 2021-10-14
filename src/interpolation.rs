45306af92b19c359d0c5aab35d77d98ca431dc58
Nil Goyette <nil.goyette@imeka.ca>
2021-10-14 11:15:11 -0400

    spline_filter always returns f64
    Although returning the original datatype seems logical, it's not a perfect solution
    because we process the data in f64 and we convert it back just before returning.
    Leaving the conversion to the caller is a better idea because:
    - the conversion is useless if the caller was going to convert it anyway.
    - the caller possibly wants the results in f64 and it will be impossible for him to
    get back the lost precision.

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -1,63 +1,60 @@
 use ndarray::{arr1, s, Array, Array1, ArrayBase, ArrayViewMut1, Axis, Data, Dimension};
-use num_traits::{AsPrimitive, Num, ToPrimitive};
+use num_traits::ToPrimitive;
 
 /// Multidimensional spline filter.
 ///
 /// The multidimensional filter is implemented as a sequence of one-dimensional spline filters. The
-/// input `data` will be processed in `f64` and converted back to the original datatype.
+/// input `data` will be processed in `f64` and returned as such.
 ///
 /// * `data` - The input N-D data.
 /// * `order` - The order of the spline.
 ///
 /// **Panics** if `order` isn't in the range \[2, 5\].
-pub fn spline_filter<S, A, D>(data: &ArrayBase<S, D>, order: usize) -> Array<A, D>
+pub fn spline_filter<S, A, D>(data: &ArrayBase<S, D>, order: usize) -> Array<f64, D>
 where
     S: Data<Elem = A>,
-    A: 'static + Clone + Copy + Num + ToPrimitive,
-    f64: AsPrimitive<A>,
+    A: ToPrimitive,
     D: Dimension,
 {
+    let mut data = data.map(|v| v.to_f64().unwrap());
     if data.len() == 1 {
-        return data.to_owned();
+        return data;
     }
 
     let poles = get_filter_poles(order);
     let gain = filter_gain(&poles);
-
-    let mut data = data.mapv(|v| v.to_f64().unwrap());
     for axis in 0..data.ndim() {
         _spline_filter1d(&mut data, Axis(axis), &poles, gain);
     }
-    data.mapv(|v| v.as_())
+    data
 }
 
 /// Calculate a 1-D spline filter along the given axis.
 ///
 /// The lines of the array along the given axis are filtered by a spline filter. The input `data`
-/// will be processed in `f64` and converted back to the original datatype.
+/// will be processed in `f64` and returned as such.
 ///
 /// * `data` - The input N-D data.
 /// * `order` - The order of the spline.
 /// * `axis` - The axis along which the spline filter is applied.
 ///
 /// **Panics** if `order` isn't in the range \[0, 5\].
-pub fn spline_filter1d<S, A, D>(data: &ArrayBase<S, D>, order: usize, axis: Axis) -> Array<A, D>
+pub fn spline_filter1d<S, A, D>(data: &ArrayBase<S, D>, order: usize, axis: Axis) -> Array<f64, D>
 where
     S: Data<Elem = A>,
-    A: 'static + Clone + Copy + Num + ToPrimitive,
-    f64: AsPrimitive<A>,
+    A: ToPrimitive,
     D: Dimension,
 {
+    let mut data = data.map(|v| v.to_f64().unwrap());
     if order == 0 || order == 1 || data.len() == 1 {
-        return data.to_owned();
+        return data;
     }
 
     let poles = get_filter_poles(order);
     let gain = filter_gain(&poles);
 
-    let mut data = data.mapv(|v| v.to_f64().unwrap());
     _spline_filter1d(&mut data, axis, &poles, gain);
-    data.mapv(|v| v.as_())
+    data
 }
 
 fn _spline_filter1d<D>(data: &mut Array<f64, D>, axis: Axis, poles: &Array1<f64>, gain: f64)
