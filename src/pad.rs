2849889a98efb9849f804a24e15b32b05f76fa8b
Nil Goyette <nil.goyette@imeka.ca>
2022-05-27 15:57:52 -0400

    n-dimensional padding

--- src/pad.rs
+++ src/pad.rs
@@ -1,22 +1,89 @@
 //! This modules defines some image padding methods for 3D images.
 
-use ndarray::{Array3, ArrayBase, Data, Ix3, ShapeBuilder};
+use ndarray::{Array, ArrayBase, Axis, Data, Dimension, NdIndex, ShapeBuilder, Slice, Zip};
 use num_traits::Zero;
 
-type PadSize = (usize, usize, usize);
+use crate::array_like;
 
+#[derive(Copy, Clone, PartialEq)]
 /// Method that will be used to select the padded values.
-pub enum PadMode {
+pub enum PadMode<T> {
+    /// Pads with a constant value.
+    ///
+    /// `[1, 2, 3] -> [T, T, 1, 2, 3, T, T]`
+    Constant(T),
+
     /// Pads with the reflection of the vector mirrored on the first and last values of the vector
     /// along each axis.
+    ///
+    /// `[1, 2, 3] -> [3, 2, 1, 2, 3, 2, 1]`
     Reflect,
 
     /// Pads with the reflection of the vector mirrored along the edge of the array.
+    ///
+    /// `[1, 2, 3] -> [2, 1, 1, 2, 3, 3, 2]`
     Symmetric,
 
     /// Pads with the wrap of the vector along the axis. The first values are used to pad the end
     /// and the end values are used to pad the beginning.
+    ///
+    /// `[1, 2, 3] -> [2, 3, 1, 2, 3, 1, 2]`
     Wrap,
+}
+
+impl<T: Copy + Zero> PadMode<T> {
+    fn init(&self) -> T {
+        match self {
+            PadMode::Constant(init) => *init,
+            _ => T::zero(),
+        }
+    }
+
+    fn action(&self) -> PadAction {
+        match self {
+            PadMode::Constant(_) => PadAction::StopAfterCopy,
+            PadMode::Reflect | PadMode::Symmetric | PadMode::Wrap => PadAction::ByIndices,
+        }
+    }
+
+    fn indices(&self, size: usize, padding: usize) -> Vec<u16> {
+        match self {
+            PadMode::Constant(_) => panic!(),
+            PadMode::Reflect => {
+                let mut v = Vec::with_capacity(2 * padding + size);
+                let size = size as u16;
+                let padding = padding as u16;
+                v.extend((1..=padding).rev());
+                v.extend(0..size);
+                v.extend((size - padding - 1..size - 1).rev());
+                v
+            }
+            PadMode::Symmetric => {
+                let mut v = Vec::with_capacity(2 * padding + size);
+                let size = size as u16;
+                let padding = padding as u16;
+                v.extend((0..padding).rev());
+                v.extend(0..size);
+                v.extend((size - padding..size).rev());
+                v
+            }
+            PadMode::Wrap => {
+                let mut v = Vec::with_capacity(2 * padding + size);
+                let size = size as u16;
+                let padding = padding as u16;
+                v.extend(size - padding..size);
+                v.extend(0..size);
+                v.extend(0..padding);
+                v
+            }
+        }
+    }
+}
+
+#[derive(PartialEq)]
+enum PadAction {
+    StopAfterCopy,
+    ByIndices,
 }
 
 /// Pad an image.
@@ -25,88 +92,38 @@ pub enum PadMode {
 /// * `pad_width` - Number of values padded to the edges of each axis.
 /// * `mode` - Method that will be used to select the padded values. See the
 ///   [`PadMode`](crate::PadMode) enum for more information.
-pub fn pad<S, A>(data: &ArrayBase<S, Ix3>, pad_width: PadSize, mode: PadMode) -> Array3<A>
+pub fn pad<S, A, D, Sh>(data: &ArrayBase<S, D>, pad: Sh, mode: PadMode<A>) -> Array<A, D>
 where
     S: Data<Elem = A>,
     A: Zero + Clone + Copy,
+    D: Dimension + Copy,
+    Sh: ShapeBuilder<Dim = D>,
+    <D as Dimension>::Pattern: NdIndex<D>,
 {
-    let indices = |size: usize, padding: usize| match mode {
-        PadMode::Reflect => reflect_indices(size, padding),
-        PadMode::Symmetric => symmetric_indices(size, padding),
-        PadMode::Wrap => wrap_indices(size, padding),
-    };
+    let pad = pad.into_shape().raw_dim().clone();
+    let new_dim = data.raw_dim() + pad.clone() + pad.clone();
+    let mut padded = array_like(&data, new_dim, mode.init());
+    let padded_dim = padded.raw_dim();
 
-    let (width, height, depth) = data.dim();
-    pad_by_indices(
-        data,
-        pad_width,
-        indices(width, pad_width.0),
-        indices(height, pad_width.1),
-        indices(depth, pad_width.2),
-    )
-}
-
-fn reflect_indices(size: usize, padding: usize) -> Vec<u16> {
-    let mut v = Vec::with_capacity(2 * padding + size);
-    let size = size as u16;
-    let padding = padding as u16;
-    v.extend((1..=padding).rev());
-    v.extend(0..size);
-    v.extend((size - padding - 1..size - 1).rev());
-    v
-}
-
-fn symmetric_indices(size: usize, padding: usize) -> Vec<u16> {
-    let mut v = Vec::with_capacity(2 * padding + size);
-    let size = size as u16;
-    let padding = padding as u16;
-    v.extend((0..padding).rev());
-    v.extend(0..size);
-    v.extend((size - padding..size).rev());
-    v
-}
-
-fn wrap_indices(size: usize, padding: usize) -> Vec<u16> {
-    let mut v = Vec::with_capacity(2 * padding + size);
-    let size = size as u16;
-    let padding = padding as u16;
-    v.extend(size - padding..size);
-    v.extend(0..size);
-    v.extend(0..padding);
-    v
-}
-
-fn pad_by_indices<S, A>(
-    data: &ArrayBase<S, Ix3>,
-    pad: PadSize,
-    indices_i: Vec<u16>,
-    indices_j: Vec<u16>,
-    indices_k: Vec<u16>,
-) -> Array3<A>
-where
-    S: Data<Elem = A>,
-    A: Zero + Clone + Copy,
-{
-    let width = data.dim().0 + 2 * pad.0;
-    let height = data.dim().1 + 2 * pad.1;
-    let depth = data.dim().2 + 2 * pad.2;
-    let mut out = Array3::zeros((width, height, depth).f());
-
-    // Using zip or Array3::from_shape_fn doesn't make this faster. Directly copying `data` into
-    // the center of `out` and only working on the borders *may* be faster, but
-    // * this solution is by far simpler and cleaner than any other
-    // * it takes around 8ms for a 100x100x100 image
-    // so lets keep it this way.
-
-    for i in 0..width {
-        let idx_i = indices_i[i] as usize;
-        for j in 0..height {
-            let idx_j = indices_j[j] as usize;
-            for k in 0..depth {
-                let idx_k = indices_k[k] as usize;
-                out[(i, j, k)] = data[(idx_i, idx_j, idx_k)];
-            }
+    if mode.action() == PadAction::StopAfterCopy {
+        // Select portion of padded array that needs to be copied from the original array.
+        let mut orig_portion = padded.view_mut();
+        for d in 0..data.ndim() {
+            orig_portion.slice_axis_inplace(Axis(d), Slice::from(pad[d]..padded_dim[d] - pad[d]));
         }
+        orig_portion.assign(data);
+        return padded;
     }
-    out
+
+    let indices: Vec<_> =
+        (0..data.ndim()).map(|d| mode.indices(data.len_of(Axis(d)), pad[d])).collect();
+    Zip::indexed(&mut padded).for_each(|idx, v| {
+        let mut idx = idx.into_shape().raw_dim().clone();
+        for d in 0..data.ndim() {
+            idx[d] = indices[d][idx[d]] as usize;
+        }
+        *v = data[idx.into_pattern()];
+    });
+
+    padded
 }
