513ba596d4c8229b54b1c1b610e0de4135caa467
Nil Goyette <nil.goyette@imeka.ca>
2022-05-31 09:58:29 -0400

    Add pad max and min

--- src/pad.rs
+++ src/pad.rs
@@ -1,6 +1,9 @@
 //! This modules defines some image padding methods for 3D images.
 
-use ndarray::{Array, ArrayBase, Axis, Data, Dimension, NdIndex, ShapeBuilder, Slice, Zip};
+use ndarray::{
+    s, Array, ArrayBase, ArrayView1, Axis, Data, Dimension, NdIndex, ShapeBuilder, Slice, Zip,
+};
+use ndarray_stats::QuantileExt;
 use num_traits::Zero;
 
 use crate::array_like;
@@ -12,6 +15,12 @@ pub enum PadMode<T> {
     ///
     /// `[1, 2, 3] -> [T, T, 1, 2, 3, T, T]`
     Constant(T),
+
+    /// Pads with the maximum value of all or part of the vector along each axis.
+    Maximum,
+
+    /// Pads with the minimum value of all or part of the vector along each axis.
+    Minimum,
 
     /// Pads with the reflection of the vector mirrored on the first and last values of the vector
     /// along each axis.
@@ -42,13 +51,24 @@ impl<T: Copy + Zero> PadMode<T> {
     fn action(&self) -> PadAction {
         match self {
             PadMode::Constant(_) => PadAction::StopAfterCopy,
+            PadMode::Maximum | PadMode::Minimum => PadAction::ByLane,
             PadMode::Reflect | PadMode::Symmetric | PadMode::Wrap => PadAction::ByIndices,
+        }
+    }
+
+    fn dynamic_value(&self, lane: ArrayView1<T>) -> T
+    where
+        T: PartialOrd,
+    {
+        match self {
+            PadMode::Minimum => *lane.min().unwrap(),
+            PadMode::Maximum => *lane.max().unwrap(),
+            _ => panic!(""),
         }
     }
 
     fn indices(&self, size: usize, padding: usize) -> Vec<u16> {
         match self {
-            PadMode::Constant(_) => panic!(),
             PadMode::Reflect => {
                 let mut v = Vec::with_capacity(2 * padding + size);
                 let size = size as u16;
@@ -76,6 +96,7 @@ impl<T: Copy + Zero> PadMode<T> {
                 v.extend(0..padding);
                 v
             }
+            _ => panic!("Only Reflect, Symmetric and Wrap have indices"),
         }
     }
 }
@@ -83,6 +104,7 @@ impl<T: Copy + Zero> PadMode<T> {
 #[derive(PartialEq)]
 enum PadAction {
     StopAfterCopy,
+    ByLane,
     ByIndices,
 }
 
@@ -95,7 +117,7 @@ enum PadAction {
 pub fn pad<S, A, D, Sh>(data: &ArrayBase<S, D>, pad: Sh, mode: PadMode<A>) -> Array<A, D>
 where
     S: Data<Elem = A>,
-    A: Zero + Clone + Copy,
+    A: Zero + Clone + Copy + PartialOrd + std::fmt::Display,
     D: Dimension + Copy,
     Sh: ShapeBuilder<Dim = D>,
     <D as Dimension>::Pattern: NdIndex<D>,
@@ -105,25 +127,41 @@ where
     let mut padded = array_like(&data, new_dim, mode.init());
     let padded_dim = padded.raw_dim();
 
-    if mode.action() == PadAction::StopAfterCopy {
+    let action = mode.action();
+    if action != PadAction::ByIndices {
         // Select portion of padded array that needs to be copied from the original array.
         let mut orig_portion = padded.view_mut();
         for d in 0..data.ndim() {
             orig_portion.slice_axis_inplace(Axis(d), Slice::from(pad[d]..padded_dim[d] - pad[d]));
         }
         orig_portion.assign(data);
-        return padded;
     }
 
-    let indices: Vec<_> =
-        (0..data.ndim()).map(|d| mode.indices(data.len_of(Axis(d)), pad[d])).collect();
-    Zip::indexed(&mut padded).for_each(|idx, v| {
-        let mut idx = idx.into_shape().raw_dim().clone();
-        for d in 0..data.ndim() {
-            idx[d] = indices[d][idx[d]] as usize;
+    match action {
+        PadAction::StopAfterCopy => padded,
+        PadAction::ByLane => {
+            for d in 0..data.ndim() {
+                let start = pad[d];
+                let end = start + data.shape()[d];
+                Zip::from(padded.lanes_mut(Axis(d))).for_each(|mut lane| {
+                    let v = mode.dynamic_value(lane.slice(s![start..end]));
+                    lane.slice_mut(s![..start]).fill(v);
+                    lane.slice_mut(s![end..]).fill(v);
+                });
+            }
+            padded
         }
-        *v = data[idx.into_pattern()];
-    });
-
-    padded
+        PadAction::ByIndices => {
+            let indices: Vec<_> =
+                (0..data.ndim()).map(|d| mode.indices(data.len_of(Axis(d)), pad[d])).collect();
+            Zip::indexed(&mut padded).for_each(|idx, v| {
+                let mut idx = idx.into_shape().raw_dim().clone();
+                for d in 0..data.ndim() {
+                    idx[d] = indices[d][idx[d]] as usize;
+                }
+                *v = data[idx.into_pattern()];
+            });
+            padded
+        }
+    }
 }
