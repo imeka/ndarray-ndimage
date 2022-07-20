1bb077b0d62c49b0afffa528ccd6be3037127ea5
Nil Goyette <nil.goyette@imeka.ca>
2022-07-20 13:25:39 -0400

    Optimize the Kernel::Full case

--- src/morphology.rs
+++ src/morphology.rs
@@ -2,6 +2,83 @@ use ndarray::{s, ArrayBase, ArrayView3, ArrayViewMut3, Data, Ix3, Zip};
 
 use crate::{array_like, dim_minus, Kernel3d, Mask};
 
+impl Kernel3d {
+    /// Erode the mask when at least one of the values doesn't respect the kernel.
+    ///
+    /// An erosion is defined either as `all(!(!w & k))` or `!any(!w & k)`. Thus, an empty kernel
+    /// will always produce a full mask.
+    #[rustfmt::skip]
+    fn erode(&self, from: ArrayView3<bool>, into: ArrayViewMut3<bool>) {
+        match *self {
+            Kernel3d::Full => {
+                // TODO This is incredibly ugly but this is much faster (3x) than the Zip
+                //Zip::from(from.windows((3, 3, 3)))
+                //    .map_assign_into(into, |w| Zip::from(w).all(|&m| m));
+                Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
+                    w[(0, 0, 0)] && w[(0, 0, 1)] && w[(0, 0, 2)]
+                 && w[(0, 1, 0)] && w[(0, 1, 1)] && w[(0, 1, 2)]
+                 && w[(0, 2, 0)] && w[(0, 2, 1)] && w[(0, 2, 2)]
+                 && w[(1, 0, 0)] && w[(1, 0, 1)] && w[(1, 0, 2)]
+                 && w[(1, 1, 0)] && w[(1, 1, 1)] && w[(1, 1, 2)]
+                 && w[(1, 2, 0)] && w[(1, 2, 1)] && w[(1, 2, 2)]
+                 && w[(2, 0, 0)] && w[(2, 0, 1)] && w[(2, 0, 2)]
+                 && w[(2, 1, 0)] && w[(2, 1, 1)] && w[(2, 1, 2)]
+                 && w[(2, 2, 0)] && w[(2, 2, 1)] && w[(2, 2, 2)]
+                })
+            }
+            Kernel3d::Star => Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
+                // This ugly condition is equivalent to
+                // *mask = !w.iter().zip(&kernel).any(|(w, k)| !w & k)
+                // but it's around 5x faster because there's no branch misprediction
+                w[(0, 1, 1)]
+                    && w[(1, 0, 1)]
+                    && w[(1, 1, 0)]
+                    && w[(1, 1, 1)]
+                    && w[(1, 1, 2)]
+                    && w[(1, 2, 1)]
+                    && w[(2, 1, 1)]
+            }),
+        }
+    }
+
+    /// Dilate the mask when at least one of the values respect the kernel:
+    ///
+    /// A dilation is defined as `any(w & k)`. Thus, an empty kernel will always produce an empty
+    /// mask.
+    #[rustfmt::skip]
+    fn dilate(&self, from: ArrayView3<bool>, into: ArrayViewMut3<bool>) {
+        match *self {
+            Kernel3d::Full => {
+                // TODO This is incredibly ugly but this is much faster (6x) than the Zip
+                //Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| w.iter().any(|&w| w))
+                Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
+                    w[(0, 0, 0)] || w[(0, 0, 1)] || w[(0, 0, 2)]
+                 || w[(0, 1, 0)] || w[(0, 1, 1)] || w[(0, 1, 2)]
+                 || w[(0, 2, 0)] || w[(0, 2, 1)] || w[(0, 2, 2)]
+                 || w[(1, 0, 0)] || w[(1, 0, 1)] || w[(1, 0, 2)]
+                 || w[(1, 1, 0)] || w[(1, 1, 1)] || w[(1, 1, 2)]
+                 || w[(1, 2, 0)] || w[(1, 2, 1)] || w[(1, 2, 2)]
+                 || w[(2, 0, 0)] || w[(2, 0, 1)] || w[(2, 0, 2)]
+                 || w[(2, 1, 0)] || w[(2, 1, 1)] || w[(2, 1, 2)]
+                 || w[(2, 2, 0)] || w[(2, 2, 1)] || w[(2, 2, 2)]
+                })
+            }
+            Kernel3d::Star => Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
+                // This ugly condition is equivalent to
+                // *mask = w.iter().zip(&kernel).any(|(w, k)| w & k)
+                // but it's around 5x faster because there's no branch misprediction
+                w[(0, 1, 1)]
+                    || w[(1, 0, 1)]
+                    || w[(1, 1, 0)]
+                    || w[(1, 1, 1)]
+                    || w[(1, 1, 2)]
+                    || w[(1, 2, 1)]
+                    || w[(2, 1, 1)]
+            }),
+        }
+    }
+}
+
 /// Binary erosion of a 3D binary image.
 ///
 /// * `mask` - Binary image to be eroded.
@@ -11,35 +88,13 @@ pub fn binary_erosion<S>(mask: &ArrayBase<S, Ix3>, kernel: Kernel3d, iterations:
 where
     S: Data<Elem = bool>,
 {
-    // Erode the mask when at least one of the values doesn't respect the kernel.
-    // An erosion is defined either as `all(!(!w & k))` or `!any(!w & k)`.
-    // An empty kernel will always produce a full mask.
-    let erode = |from: ArrayView3<bool>, into: ArrayViewMut3<bool>| {
-        if kernel == Kernel3d::Full {
-            Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| !w.iter().any(|w| !w));
-        } else {
-            Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
-                // This ugly condition is equivalent to
-                // *mask = !w.iter().zip(&kernel).any(|(w, k)| !w & k)
-                // but it's around 5x faster because there's no branch misprediction
-                !(!w[(0, 1, 1)]
-                    || !w[(1, 0, 1)]
-                    || !w[(1, 1, 0)]
-                    || !w[(1, 1, 1)]
-                    || !w[(1, 1, 2)]
-                    || !w[(1, 2, 1)]
-                    || !w[(2, 1, 1)])
-            });
-        }
-    };
-
     // By definition, all borders are set to 0
     let (width, height, depth) = dim_minus(mask, 1);
     let mut eroded_mask = Mask::from_elem(mask.dim(), false);
     let zone = s![1..width, 1..height, 1..depth];
     eroded_mask.slice_mut(zone).assign(&mask.slice(zone));
 
-    erode(mask.view(), eroded_mask.slice_mut(zone));
+    kernel.erode(mask.view(), eroded_mask.slice_mut(zone));
 
     if iterations > 1 {
         let mut previous = eroded_mask.clone();
@@ -49,7 +104,7 @@ where
             let (width, height, depth) = dim_minus(mask, it);
             let zone = s![it..width, it..height, it..depth];
 
-            erode(from, eroded_mask.slice_mut(zone));
+            kernel.erode(from, eroded_mask.slice_mut(zone));
 
             if it != iterations {
                 previous = eroded_mask.clone();
@@ -69,37 +124,17 @@ pub fn binary_dilation<S>(mask: &ArrayBase<S, Ix3>, kernel: Kernel3d, iterations
 where
     S: Data<Elem = bool>,
 {
-    // Dilate the mask when at least one of the values respect the kernel: `any(w & k)`.
-    // Note that an empty kernel will always produce an empty mask.
-    let dilate = |from: ArrayView3<bool>, into: ArrayViewMut3<bool>| {
-        if kernel == Kernel3d::Full {
-            Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| w.iter().any(|&w| w));
-        } else {
-            Zip::from(from.windows((3, 3, 3))).map_assign_into(into, |w| {
-                // This ugly condition is equivalent to
-                // *mask = w.iter().zip(&kernel).any(|(w, k)| w & k)
-                // but it's around 5x faster because there's no branch misprediction
-                w[(0, 1, 1)]
-                    || w[(1, 0, 1)]
-                    || w[(1, 1, 0)]
-                    || w[(1, 1, 1)]
-                    || w[(1, 1, 2)]
-                    || w[(1, 2, 1)]
-                    || w[(2, 1, 1)]
-            });
-        }
-    };
     let (width, height, depth) = mask.dim();
     let crop = s![1..=width, 1..=height, 1..=depth];
     let mut new_mask = array_like(mask, (width + 2, height + 2, depth + 2), false);
     new_mask.slice_mut(crop).assign(mask);
 
     let mut previous = new_mask.clone();
-    dilate(previous.view(), new_mask.slice_mut(crop));
+    kernel.dilate(previous.view(), new_mask.slice_mut(crop));
 
     for _ in 1..iterations {
         previous = new_mask.clone();
-        dilate(previous.view(), new_mask.slice_mut(crop));
+        kernel.dilate(previous.view(), new_mask.slice_mut(crop));
     }
 
     new_mask.slice(crop).to_owned()
