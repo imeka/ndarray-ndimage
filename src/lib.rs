d78625c12da1bb7cc7b62a565d9917716535114c
Nil Goyette <nil.goyette@imeka.ca>
2022-09-08 09:36:27 -0400

    Refactor filters

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,8 @@ mod pad;
 
 pub use filters::{
     con_corr::{convolve, convolve1d, correlate, correlate1d},
-    gaussian_filter, gaussian_filter1d, median_filter,
+    gaussian::{gaussian_filter, gaussian_filter1d},
+    median::median_filter,
     min_max::{
         maximum_filter, maximum_filter1d, maximum_filter1d_to, minimum_filter, minimum_filter1d,
         minimum_filter1d_to,
