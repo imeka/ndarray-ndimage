7441a5781f17dd23ba09370c8d797b16caa58666
Nil Goyette <nil.goyette@imeka.ca>
2022-09-09 11:54:39 -0400

    Add tests

--- tests/filters.rs
+++ tests/filters.rs
@@ -3,7 +3,8 @@ use ndarray::{arr1, arr2, s, Array1, Array2, Axis};
 
 use ndarray_ndimage::{
     convolve, convolve1d, correlate, correlate1d, gaussian_filter, maximum_filter,
-    maximum_filter1d, median_filter, minimum_filter, minimum_filter1d, prewitt, BorderMode, Mask,
+    maximum_filter1d, median_filter, minimum_filter, minimum_filter1d, prewitt, sobel, BorderMode,
+    Mask,
 };
 
 #[test] // Results verified with SciPy. (v1.9.0)
@@ -704,6 +705,46 @@ fn test_prewitt() {
             [-0.3, 0.4, -2.4, -0.4, 3.3, 0.6],
             [-2.8, -3.0, 3.9, 1.4, -3.7, -1.0],
             [-5.4, -9.2, 10.2, 6.3, -10.1, -2.4]
+        ]),
+        epsilon = 1e-5
+    );
+}
+
+#[test] // Results verified with SciPy. (v1.9.0)
+fn test_sobel() {
+    let a = arr1(&[2.0, 8.1, 0.5, 4.0, 1.1, 9.0, 9.0, 0.8]);
+    assert_relative_eq!(
+        sobel(&a, Axis(0), BorderMode::Reflect),
+        arr1(&[6.1, -1.5, -4.1, 0.6, 5.0, 7.9, -8.2, -8.2]),
+        epsilon = 1e-5
+    );
+
+    let matrix = arr2(&[
+        [1.5, 2.3, 0.7, 1.1, 6.0, 1.7],
+        [0.5, 1.3, 0.0, 0.1, 1.2, 0.7],
+        [0.4, 1.3, 2.7, 0.1, 0.8, 0.1],
+        [2.1, 0.1, 0.7, 0.1, 1.0, 2.8],
+        [5.7, 4.0, 1.8, 9.1, 4.8, 2.7],
+    ]);
+    assert_relative_eq!(
+        sobel(&matrix, Axis(0), BorderMode::Reflect),
+        arr2(&[
+            [-4.0, -3.7, -3.4, -7.5, -11.6, -7.8],
+            [-4.3, -1.1, 2.0, -5.2, -13.0, -10.0],
+            [3.6, -0.1, 0.2, 0.5, 1.7, 6.1],
+            [18.6, 9.8, 9.9, 21.1, 19.6, 11.8],
+            [14.7, 12.5, 15.1, 22.9, 16.5, 3.5]
+        ]),
+        epsilon = 1e-5
+    );
+    assert_relative_eq!(
+        sobel(&matrix, Axis(1), BorderMode::Reflect),
+        arr2(&[
+            [3.2, -2.9, -4.8, 17.1, 2.4, -13.4],
+            [3.3, 0.5, -4.8, 5.8, 1.8, -6.0],
+            [0.6, 2.7, -3.6, -2.3, 3.3, -0.1],
+            [-4.8, -4.4, 3.9, 1.7, -1., 0.8],
+            [-7.1, -13.1, 15.3, 9.3, -16.5, -4.5]
         ]),
         epsilon = 1e-5
     );
